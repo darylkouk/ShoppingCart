0da0fef7e65b15a7ab757c3d710ca40bcf9cf066
darylkouk <57950357+darylkouk@users.noreply.github.com>
2020-04-08 19:16:32 +0800

    Added Search, Product Recomendation, List Product review but need to refine more

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -31,13 +31,35 @@ namespace ShoppingCart.Areas.Customer.Controllers
         public IActionResult ViewProduct([FromServices] DataContext dbcontext, string selected)
         {
             //Product selectedProduct = dbcontext.products.Where(x => x.Name == selected).FirstOrDefault();
+            //List<ProductDetail> selectedProductDetails = dbcontext.productDetails.Where(x => x.ProductId == selectedProduct.Id).ToList();
             //List<Product> recommendedProducts = dbcontext.products.Where(x => x.Genre == selectedProduct.Genre).ToList();
             Product selectedProduct = dbcontext.products.Where(x => x.Name.ToLower() == "borderland 3").FirstOrDefault();
-            List<Product> recommendedProducts = dbcontext.products.Where(x => x.Genre.ToLower() == "shooter").ToList();
+            List<ProductDetail> selectedProductDetails = dbcontext.productDetails.Where(x => x.ProductId == selectedProduct.Id).ToList();
+            List<Product> recommendedProducts = dbcontext.products.Where(x => x.Genre.ToLower() == "shooter" && x.Name != selectedProduct.Name).ToList();
+
 
             ViewData["selectedProduct"] = selectedProduct;
             ViewData["recommendedProducts"] = recommendedProducts;
+
+            if (selectedProductDetails.Count() == 0)
+            {
+                ViewData["Rating"] = 0;
+            }
+            else
+            {
+                ViewData["selectedProductDetails"] = selectedProductDetails;
+                ViewData["Rating"] = Average(selectedProductDetails);
+            }
             return View();
+        }
+        public int Average(List<ProductDetail> productDetail)
+        {
+            int aveRating = 0;
+            foreach (var select in productDetail)
+            {
+                aveRating += select.Rating;
+            }
+            return aveRating = aveRating / productDetail.Count();
         }
 
         public IActionResult Index()
