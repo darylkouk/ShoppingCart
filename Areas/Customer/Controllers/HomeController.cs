5dd59beb0712a47cfb9a7c1e80f019952bd0d5e2
darylkouk <57950357+darylkouk@users.noreply.github.com>
2020-04-08 16:51:01 +0800

    Added View Product Details

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ShoppingCart.Data;
 using ShoppingCart.Models;
 
 namespace ShoppingCart.Areas.Customer.Controllers
@@ -17,6 +18,26 @@ namespace ShoppingCart.Areas.Customer.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }
+
+        //Search function
+        public IActionResult Search([FromServices] DataContext dbcontext, string searchInput)
+        {
+            List<Product> products = dbcontext.products.Where(x => x.Name.Contains(searchInput) || x.Description.Contains(searchInput)).ToList();
+            ViewData["search"] = products;
+            return View("Gallery");
+        }
+        //View Product Details
+        public IActionResult ViewProduct([FromServices] DataContext dbcontext, string selected)
+        {
+            //Product selectedProduct = dbcontext.products.Where(x => x.Name == selected).FirstOrDefault();
+            //List<Product> recommendedProducts = dbcontext.products.Where(x => x.Genre == selectedProduct.Genre).ToList();
+            Product selectedProduct = dbcontext.products.Where(x => x.Name.ToLower() == "borderland 3").FirstOrDefault();
+            List<Product> recommendedProducts = dbcontext.products.Where(x => x.Genre.ToLower() == "shooter").ToList();
+
+            ViewData["selectedProduct"] = selectedProduct;
+            ViewData["recommendedProducts"] = recommendedProducts;
+            return View();
         }
 
         public IActionResult Index()
