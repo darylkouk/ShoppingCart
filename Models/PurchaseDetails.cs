d92e859640be7e7f7978d9e873929cf665337ea7
darylkouk <57950357+darylkouk@users.noreply.github.com>
2020-04-09 18:15:07 +0800

    test

--- Models/PurchaseDetails.cs
+++ Models/PurchaseDetails.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-
+//test
 namespace ShoppingCart.Models
 {
     public class PurchaseDetails
