ada9c467620549f353b628493298949d96602666
Hong Yi Ng <e0533409@u.nus.edu>
2020-04-09 20:26:12 +0800

    added a new 'seeder'

---
+++ Data/PuchaseUpdates.cs
@@ -0,0 +1,38 @@
+﻿using Scrypt;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data
+{
+    public class PuchaseUpdates
+    {
+        protected DataContext dbcontext;
+        public PuchaseUpdates(DataContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+        public string AddNewPurchase(string productid, string userid, DateTime createddate)
+        {
+            PurchaseDetails newpurchase = new PurchaseDetails()
+            {
+                Id = Guid.NewGuid().ToString(),
+                ActivationCode = Guid.NewGuid().ToString(),
+                ProductId = productid,
+                UserId = userid,
+                CreatedDate = createddate
+            };
+
+            return newpurchase.ActivationCode;
+        }
+
+        public List<PurchaseDetails> ViewPastTransaction(string userid)
+        {
+            return dbcontext.purchaseDetails.Where(x => x.UserId == userid).ToList();
+        }
+
+        
+    }
+}
