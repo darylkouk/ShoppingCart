cc4c3650ccaeeff5841264ce37ea134b4b71579e
WuttYeeHL <63301178+WuttYeeHL@users.noreply.github.com>
2020-04-09 17:46:16 +0800

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Scrypt;
+using ShoppingCart.Data;
+
+namespace ShoppingCart.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index", "Home");
+            //return View();
+        }
+        public IActionResult Validate([FromServices] DataContext dbcontext, string username, string password)
+        {
+            ScryptEncoder encoder = new ScryptEncoder();
+            if (username == null || password == null)
+            {
+                ViewData["fail"] = "All Fields Required";
+                return View("Login");
+            }
+            var usernameValidate = dbcontext.users.Where(x => x.Username == username).FirstOrDefault();
+
+            if (usernameValidate == null)
+            {
+                ViewData["fail"] = "Invalid Username or Password";
+                return View("Login");
+            }
+            else
+            {
+                bool passwordValidate = encoder.Compare(password, usernameValidate.Password);
+                if (passwordValidate)
+                {
+                    HttpContext.Session.SetString("username", username);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewData["fail"] = "Invalid Username or Password";
+                    return View("Login");
+                }
+            }
+        }
+        public IActionResult PurchasedHistory()
+        {
+            ViewData["username"] = HttpContext.Session.GetString("username");
+            return View();
+        }
+    }
+}
