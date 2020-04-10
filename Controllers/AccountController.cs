c28d8e82c303916f0263b48410b0f585c1f3f80d
Hong Yi Ng <e0533409@u.nus.edu>
2020-04-10 19:10:32 +0800

    redid the accountcontroller -action purchase history (trying to get the drop down list to work and succeeded)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -113,26 +113,98 @@ namespace ShoppingCart.Controllers
 
         public IActionResult PurchaseHistory([FromServices] DataContext dbcontext)
         {
-            ViewData["username"] = HttpContext.Session.GetString("username");
+            ViewData["username"] = "daryl";
+            //HttpContext.Session.GetString("username");
 
-            if (ViewData["username"] == null)
-            {
-                return View("Login");
-            }
+            //if (ViewData["username"] == null)
+            //{
+            //    return View("Login");
+            //}
 
+            
             List<PurchaseDetails> history = dbcontext.purchaseDetails.Where(x => x.UserId == dbcontext.users.Where(x => x.Username == ViewData["username"] as string).FirstOrDefault().Id).ToList();
 
-            IEnumerable<PurchaseDetails> sortedhistory = from his in history orderby his.ProductId select his;
-
+            IEnumerable<PurchaseDetails> sortedhistory = from his in history 
+                                                         orderby his.Product.Name, his.CreatedDate
+                                                         select his;
+           
             List<PurchaseDetails> sortedhistorylist = new List<PurchaseDetails>();
 
             foreach (PurchaseDetails x in sortedhistory)
             {
                 sortedhistorylist.Add(x);
             }
-            
-            ViewData["purchase history"] = sortedhistorylist;
-            
+
+            List<string> totalproductname = new List<string>();
+            List<string> totalactivationcode = new List<string>();
+            List<DateTime> totalcreateddate = new List<DateTime>();
+
+            for (int i = 0; i < sortedhistorylist.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    totalproductname.Add(sortedhistorylist[i].Product.Name);
+                    totalactivationcode.Add(sortedhistorylist[i].ActivationCode);
+                    totalcreateddate.Add(sortedhistorylist[i].CreatedDate);
+                }
+                                
+                
+                else if (i > 0 && sortedhistorylist[i].CreatedDate == sortedhistorylist[i - 1].CreatedDate && sortedhistorylist[i].Product.Name == sortedhistorylist[i - 1].Product.Name)
+                {
+
+                    totalactivationcode[i - 1] += " " + sortedhistorylist[i].ActivationCode;
+                }
+                else
+                {
+                    totalproductname.Add(sortedhistorylist[i].Product.Name);
+                    totalactivationcode.Add(sortedhistorylist[i].ActivationCode);
+                    totalcreateddate.Add(sortedhistorylist[i].CreatedDate);
+                }
+
+            }
+
+
+            ////Martin from here on it don't work overall because of the limitation in purchasedetails.activationcode
+            //List<PurchaseDetails> sortedandgroupedhistorylist = new List<PurchaseDetails>();
+
+
+            //for (int i = 0; i<sortedhistorylist.Count();i++)
+            //{                
+            //    if (i == 0)
+            //    {
+            //        PurchaseDetails temphis = new PurchaseDetails()
+            //        {                        
+            //            ProductId = sortedhistorylist[i].ProductId,
+            //            ActivationCode = sortedhistorylist[i].ActivationCode,
+            //            CreatedDate = sortedhistorylist[i].CreatedDate                       
+            //        };
+
+            //        sortedandgroupedhistorylist.Add(temphis);
+            //    }
+            //    else if (i > 0 && sortedhistorylist[i].CreatedDate == sortedhistorylist[i - 1].CreatedDate && sortedhistorylist[i].Product.Name == sortedhistorylist[i - 1].Product.Name)
+            //    {
+
+            //        sortedhistorylist[i - 1].ActivationCode += " " + sortedhistorylist[i].ActivationCode;
+            //    }
+            //    else
+            //    {
+            //        PurchaseDetails temphis = new PurchaseDetails()
+            //        {
+            //            ProductId = sortedhistorylist[i].ProductId,
+            //            ActivationCode = sortedhistorylist[i].ActivationCode,
+            //            CreatedDate = sortedhistorylist[i].CreatedDate
+            //        };                    
+            //        sortedandgroupedhistorylist.Add(temphis);
+            //    }
+
+            //}
+
+            //ViewData["purchase history"] = sortedandgroupedhistorylist;
+
+            ViewData["productnames"] = totalproductname;
+            ViewData["activationcodes"] = totalactivationcode;
+            ViewData["createddates"] = totalcreateddate;
+
             return View();
         }
 
