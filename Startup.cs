bbdfe75562873c5ff9479290fb64e3f9dbc5cdb6
darylkouk <57950357+darylkouk@users.noreply.github.com>
2020-04-11 15:16:20 +0800

    Bug Fixed

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace ShoppingCart_controller
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=index}/{id?}");
+                    pattern: "{controller=Home}/{action=Gallery}/{id?}");
             });
 
             dbcontext.Database.EnsureDeleted();
